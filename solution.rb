964817a61eb758433b751c1d521113f9f33df23d
Wayne del Corral Jr <wdelcorjr@gmail.com>
2020-08-28 11:14:27 -0500

    build Agent and Seller classes

--- solution.rb
+++ solution.rb
@@ -1,13 +1,15 @@
 class Listing
     attr_reader :location
-    attr_accessor :status, :price
+    attr_accessor :status, :price, :agent, :seller
 
     @@all = []
 
-    def initialize(location, price, status="for sale")
+    def initialize(location, price, agent, seller, status="for sale")
         @location = location
         @price = price
         @status = status
+        @agent = agent
+        @seller = seller
         @@all << self
     end
 
@@ -19,4 +21,41 @@ class Listing
         @@all
     end
 
+    def seller
+        
+    end
+
 end
+
+class Agent
+    attr_reader :name
+    attr_accessor :telephone, :yrs_experience
+    def initialize(name, telephone, yrs_experience)
+        @name = name
+        @telephone = telephone
+        @yrs_experience = yrs_experience
+    end
+    def listings
+        Listing.all.select {|listing| listing.agent == self}
+    end
+    def sellers
+        self.listings.map {|listing| listing.seller}
+    end
+end
+
+class Seller
+    attr_reader :name
+    def initialize(name)
+        @name = name
+    end
+    def agent
+        self.listing.agent
+    end
+    def listings
+        
+    end
+end
+#Listing belongs to Agent, Agent has many Listings
+#Listing belongs to Seller, Seller has a Listing, 
+#Agent has many Sellers through Listing
+#Seller has Agent through Listing 
